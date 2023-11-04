6ba962a03085dce026eb3a4b2bcb3d694b30ee51
royhairul <royhairulanam19@gmail.com>
2023-11-04 23:10:58 +0700

    Change to using ListView builder

--- lib/ui/data_view.dart
+++ lib/ui/data_view.dart
@@ -1,23 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:form_app/ui/person.dart';
 
 class ListData extends StatefulWidget {
-  final String? fullName;
-  final String? email;
-  final String? personalId;
-  final String? phone;
-  final String? address;
-  final String? date;
+  late List<Person> persons;
 
-  const ListData({
-    Key? key,
-    this.fullName,
-    this.email,
-    this.personalId,
-    this.phone,
-    this.address,
-    this.date
-
-  }) : super(key: key);
+  ListData({required this.persons});
 
   @override
   _ListDataState createState() => _ListDataState();
@@ -26,7 +13,6 @@ class ListData extends StatefulWidget {
 class _ListDataState extends State<ListData> {
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: const Text('LIST PERSONAL DATA')
@@ -34,73 +20,22 @@ class _ListDataState extends State<ListData> {
       body: Container(
         alignment: Alignment.topLeft,
         margin: const EdgeInsets.only(top: 20),
-        child : Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-              _showKodeProduk(screenWidth)
-            ]
-            .map((widget) => Padding(
-                padding: const EdgeInsets.only(left: 30),
-                child: widget
-              )).toList()
-            )
+        child : Center(
+          child: ListView.builder(
+            itemCount: widget.persons.length,
+            itemBuilder: (context, index) {
+              return Card(
+                elevation: 5, // Tambahkan elevasi untuk efek bayangan
+                margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5), // Tambahkan margin
+                child: ListTile(
+                  title: Text(widget.persons[index].fullName),
+                  subtitle: Text(widget.persons[index].address)
+                ),
+              );
+            }
           )
-      );
-    }
-
-
-  _showKodeProduk(sWidth) {
-    return Container(
-      width: (sWidth - 55.0),
-      padding: EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.black, width: 2.0), // Border
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey,
-            offset: Offset(0, 2),
-            blurRadius: 6.0,
-          ),
-        ], // Shadow
-        borderRadius: BorderRadius.circular(12.0), // Border radius
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min, 
-        children: [
-          Text(
-            widget.fullName.toString(),
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 18,
-                fontFamily: 'Chivo',
-                fontWeight: FontWeight.w400,
-                height: 0,
-            ),
-          ),
-          Text(
-            widget.address.toString(),
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 10,
-                fontFamily: 'Chivo',
-                fontWeight: FontWeight.w400,
-                height: 0,
-            ),
-          ),
-          Text(
-              'Email : ${widget.email}',
-              style: TextStyle(
-                color: Color(0xFF4A3CE4),
-                fontSize: 10,
-                fontFamily: 'Chivo',
-                fontWeight: FontWeight.w400,
-                height: 0,
-              ),
-          )
-        ]
+        )
       )
     );
   }
-
 }
