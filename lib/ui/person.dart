cb24484082cb7dc4aa3cb634866ae37c4d40f342
royhairul <royhairulanam19@gmail.com>
2023-11-04 23:11:42 +0700

    Add new file for class Person

---
+++ lib/ui/person.dart
@@ -0,0 +1,17 @@
+class Person {
+  late String fullName;
+  late String email;
+  late String phoneNumber;
+  late String personalId;
+  late String address;
+  late String birthDate;
+
+  Person({
+    required this.fullName,
+    required this.email,
+    required this.phoneNumber,
+    required this.personalId,
+    required this.address,
+    required this.birthDate,
+  });
+}
